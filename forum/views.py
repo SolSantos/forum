e19e38a1142071a36fab6147c6a90cc145e8d953
Daniel Margarido <drmargarido@gmail.com>
2018-10-24 02:01:15 +0100

    Added login and logout views

--- forum/views.py
+++ forum/views.py
@@ -18,7 +18,8 @@ def do_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect("/")
+    
+    return redirect("/")
 
 
 def do_logout(request):

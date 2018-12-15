943418ebb94fc4d52cdbf23ee549b62651bbf212
Solange Gomes Santos <solangegs@student.dei.uc.pt>
2018-12-15 19:58:11 +0000

    Alterações no visual da página de login e homepage. Confirmação antes de eliminar.

---
+++ forum/static/forum/js/eliminar_pergunta.js
@@ -0,0 +1,17 @@
+var selected_trash_id = -1;
+
+$(".lixo").click(function () {
+    $("#confirm").css("opacity", "1").css("visibility", "visible");
+    selected_trash_id = this.id.split("_")[1];
+});
+
+function hide_popup(){
+    $("#confirm").css("opacity", "0").css("visibility", "hidden");
+}
+
+$("#no_button").click(hide_popup);
+
+$("#yes_button").click(function () {
+    location.href = "/delete_question/" + selected_trash_id + "/";
+    hide_popup();
+});
